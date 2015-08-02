694b7a05826e2cf145e5c8820df5339b6999b32e
MeowTheCat <miaomiao@xiaomiaomiaos-MacBook-Air.local>
2015-08-01 22:04:48 -0700

    test

--- uploadpic.php
+++ uploadpic.php
@@ -2,33 +2,6 @@
 require 'aws.phar';
 include_once('easyphpthumbnail.class.php');
 date_default_timezone_set('America/Los_Angeles');
-//copy('http://www.forever21.com/images/default_750/00172357-03.jpg', 'file.jpg');
-$old_file = 'file.jpg';
-
-$thumb = new easyphpthumbnail;
-
-$xSize = getimagesize($old_file)[0];
-$ySize = getimagesize($old_file)[1];
-
-$crop_left = 0;
-$crop_right = 0;
-$crop_top = 0;
-$crop_bottom = 0;
-
-if($ySize/$xSize > 3.5/2.5) { $crop_top = ($ySize - $xSize*3.5/2.5)/2; $crop_bottom = ($ySize - $xSize*3.5/2.5)/2;}
-if($ySize/$xSize < 3.5/2.5) { $crop_left = ($xSize - $ySize*2.5/3.5)/2; $crop_right =($xSize - $ySize*2.5/3.5)/2;}
-
-$thumb -> Cropimage = array(1,1,$crop_left,$crop_right,$crop_top,$crop_bottom);
-//$thumb -> Backgroundcolor = '#123456';
-//$thumb -> Clipcorner = array(2,5,0,1,1,1,1);
-//$thumb -> Maketransparent = array(1,0,'#123456',0);
-
-$thumb -> Borderpng = 'frame_red.png';
-$thumb -> Thumbsize = 100;
-$thumb -> Percentage = true;
-$thumb -> Thumbfilename = 'newfile.jpg';
-//$thumb -> Thumbsaveas ="png";  
-$thumb -> Createthumb($old_file,'file');
 
 
 use Aws\S3\S3Client;
@@ -41,22 +14,8 @@ $s3 = new S3Client([
     ],
 ]);
 
-$bucket = 'lumiaomiao-macy';
 
-// $filepath should be absolute path to a file on disk						
-$filename = 'newfile.jpg';
-					
-// Upload a file.
-$result = $s3->putObject(array(
-    'Bucket'       => $bucket,
-    'Key'          => $filename,
-    'SourceFile'   => $filename,
-    'ContentType'  => 'image/jpeg',
-    'StorageClass' => 'STANDARD'
- 
-));
 
-//if(unlink("newfile.jpg")) echo '##'; else echo "44";
 
 $mysqli = new mysqli("deal.cpg8bvjgkezo.us-east-1.rds.amazonaws.com", "root", "Linshuizhaohua!", "blackjack");
 if ($mysqli->connect_errno) {
@@ -67,17 +26,72 @@ $res = $mysqli->query("select  product_id, sku_number, product_url, image_url, s
 
 
 $res->data_seek(0);
+$n=0;
 
+$old_file = 'file.jpg';
+
+//Loppppppppppppp.....
 while ($row = $res->fetch_assoc()) 
 { 
+    $image_url = str_replace("wid=300","wid=600",$row['image_url']);
+    copy($image_url, $old_file );
+
+
+    $thumb = new easyphpthumbnail;
+
+    $xSize = getimagesize($old_file)[0];
+    $ySize = getimagesize($old_file)[1];
+
+    $crop_left = 0;
+    $crop_right = 0;
+    $crop_top = 0;
+    $crop_bottom = 0;
+
+    if($ySize/$xSize > 3.5/2.5) { $crop_top = ($ySize - $xSize*3.5/2.5)/2; $crop_bottom = ($ySize - $xSize*3.5/2.5)/2;}
+    if($ySize/$xSize < 3.5/2.5) { $crop_left = ($xSize - $ySize*2.5/3.5)/2; $crop_right =($xSize - $ySize*2.5/3.5)/2;}
+    $thumb -> Cropimage = array(1,1,$crop_left,$crop_right,$crop_top,$crop_bottom);
+ 
+    $thumb -> Borderpng = 'frame_red.png';
+    $thumb -> Thumbsize = 100;
+    $thumb -> Percentage = true;
+    $thumb -> Thumbfilename = 'newfile.jpg';
+    $thumb -> Createthumb($old_file,'file');
+
+
+    // Upload a file.
+  try{
+    $s3_result = $s3->putObject(array(
+        'Bucket'       => 'lumiaomiao-macy',
+        'Key'          => $row['sku_number'],
+        'SourceFile'   => 'newfile.jpg',
+        'ContentType'  => 'image/jpg',
+        'StorageClass' => 'STANDARD'
+     
+    ));
+    echo $s3_result['ObjectURL'] . "\n";
+   }
+  catch (S3Exception $e) 
+    {
+        echo $e->getMessage() . "\n";
+    }
+
+    unlink("newfile.jpg");
+
+  
+ 
     $query = 'replace into final (product_id, sku_number, product_url,sale_price,retail_price, brand, attribute_2) values ("'.$row['product_id'].'","'.$row['sku_number'].'","'.$row['product_url'].'",'.$row['sale_price'].','.$row['retail_price'].',"'.$row['brand'].'","'.$row['attribute_2'].'")';
     
     if(!$mysqli->query($query)) {echo "error :".$query; return;}
 
-    $mysqli->query($query);
+
+    $n++;
+
+
+    //echo $n."\n";
+    
  
 
-}
+} //loop..............
 
 
 ?>
