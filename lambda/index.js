c5b564b70f9cc109e9537d3a7e82e3311713daa4
MeowTheCat <miaomiao@xiaomiaomiaos-MacBook-Air.local>
2015-08-02 13:22:40 -0700

    rewqr

--- lambda/index.js
+++ lambda/index.js
@@ -19,9 +19,9 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
-connection.query('select CEILING(retail_price) retail_price, CEILING(sale_price) sale_price,s3_url from final order by rand() limit 1 ', function(err, rows, fields) {
+connection.query('select CEILING(retail_price) retail_price, CEILING(sale_price) sale_price,s3_url,product_url,brand,attribute_2 from final order by rand() limit 1 ', function(err, rows, fields) {
   if (err)  { context.fail (err); }
-  var result = {"image": rows[0].s3_url, "retail_price": rows[0].retail_price, "sale_price": rows[0].sale_price};
+  var result = {"image": rows[0].s3_url, "retail_price": rows[0].retail_price, "sale_price": rows[0].sale_price, "brand": rows[0].brand, "category": rows[0].attribute_2, "url": rows[0].product_url};
   context.succeed(result);  
 });
 
