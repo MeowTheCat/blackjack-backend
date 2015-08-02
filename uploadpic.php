77278857a5de2dbaba332c42507a395abb395021
MeowTheCat <miaomiao@xiaomiaomiaos-MacBook-Air.local>
2015-08-01 19:20:12 -0700

    fadfs

--- uploadpic.php
+++ uploadpic.php
@@ -10,13 +10,10 @@ $thumb = new easyphpthumbnail;
 $xSize = getimagesize($old_file)[0];
 $ySize = getimagesize($old_file)[1];
 
-echo $xSize; echo $ySize;
-
 $crop_left = 0;
 $crop_right = 0;
 $crop_top = 0;
 $crop_bottom = 0;
-
 
 if($ySize/$xSize > 3.5/2.5) { $crop_top = ($ySize - $xSize*3.5/2.5)/2; $crop_bottom = ($ySize - $xSize*3.5/2.5)/2;}
 if($ySize/$xSize < 3.5/2.5) { $crop_left = ($xSize - $ySize*2.5/3.5)/2; $crop_right =($xSize - $ySize*2.5/3.5)/2;}
@@ -61,8 +58,26 @@ $result = $s3->putObject(array(
 
 //if(unlink("newfile.jpg")) echo '##'; else echo "44";
 
+$mysqli = new mysqli("deal.cpg8bvjgkezo.us-east-1.rds.amazonaws.com", "root", "Linshuizhaohua!", "blackjack");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$res = $mysqli->query("select  product_id, sku_number, product_url, image_url, sale_price, retail_price, brand, attribute_2 from raw where availability='in-stock' and sale_price>0 and sale_price/retail_price <=0.7 and attribute_2 in ('dress','jacket','coat', 'top','skirt','suit','sweater') group by sku_number ");
 
 
+$res->data_seek(0);
+
+while ($row = $res->fetch_assoc()) 
+{ 
+    $query = 'replace into final (product_id, sku_number, product_url,sale_price,retail_price, brand, attribute_2) values ("'.$row['product_id'].'","'.$row['sku_number'].'","'.$row['product_url'].'",'.$row['sale_price'].','.$row['retail_price'].',"'.$row['brand'].'","'.$row['attribute_2'].'")';
+    
+    if(!$mysqli->query($query)) {echo "error :".$query; return;}
+
+    $mysqli->query($query);
+ 
+
+}
 
 
 ?>
